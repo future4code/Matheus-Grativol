11e56772c11c1e1c3132e308e476e1e4dd98f16d
Matheus Grativol <matheus_grati@hotmail.com>
2021-09-14 21:04:36 -0300

    Exercicio de Strings e Arrays

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,74 @@
+
+
+// Exercícios de interpretação de código 1
+
+// console.log('a. ', array) = a. undefined
+
+//  console.log('b. ', array) = b. null
+
+//  console.log('c. ', array.length) = c. 11
+
+//  console.log('d. ', array[i]) = d. 0
+
+//  console.log('e. ', array) = e. 19 na posição 1
+
+//  console.log('f. ', valor) = f. array na posição 6
+
+// Exercícios de interpretação de código 2
+
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+// Exercícios de escrita de código
+
+/*const nomeUsuario = prompt("Qual o seu nome?")
+const emailUsuario = prompt("Qual o Eamil?")
+//const fraseUsuario = `O e-mail ${emailUsuario} foi cadastrado com sucesso. seja bem-vinda(o), ${nomeUsuario} ` //opção
+//console.log(fraseUsuario)
+console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. seja bem-vinda(o), ${nomeUsuario} `)
+*/
+
+// const minhaComidaFavorita = ["doce de abacaxi", "hamburguer", "churrasco", "pipoca doce", "pizza" ]
+// console.log(minhaComidaFavorita)
+// console.log(`Essas são as minhas comidas preferidas: 
+// ${minhaComidaFavorita[0]}
+// ${minhaComidaFavorita[1]}
+// ${minhaComidaFavorita[2]}
+// ${minhaComidaFavorita[3]}
+// ${minhaComidaFavorita[4]}
+// `)
+
+// const comidaPreferidaUsuario = prompt("Qual a sua comida prederida?")
+// const trocaDeComida = minhaComidaFavorita.splice(1,1, comidaPreferidaUsuario)
+
+// console.log(minhaComidaFavorita)
+
+
+// const listaDeTarefas = []
+
+// const tarefa1 = prompt("Uma tarefa do dia?")
+// const tarefa2 = prompt("Uma tarefa do dia?")
+// const tarefa3 = prompt("Uma tarefa do dia?")
+
+// listaDeTarefas.push(tarefa1 , tarefa2, tarefa3)
+// console.log(listaDeTarefas)
+
+// const atividadeRealizada = prompt("Digite o indice de uma tarefa realizada 0,1 ou 2?")
+// const remocaoTarefa = listaDeTarefas.splice(atividadeRealizada , 1)
+
+// console.log(listaDeTarefas)
+
+
+// Desafio
+
+// const frase = prompt("Diga uma frase")
+// const array = []
+// array.push(frase)
+
+// // console.log(array)
+// console.log(frase.split(" "))
+
+// const array = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+// console.log(array.indexOf("Abacaxi"))
+// console.log(array.length)
