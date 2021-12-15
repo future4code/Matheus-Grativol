969cb7aeb222da23a0a57fc4446c26f7c6a09836
Matheus Grativol <matheus_grati@hotmail.com>
2021-12-14 21:58:57 -0300

    Nada ainda!

---
+++ modulo3/projeto-labefy/projeto-labefy/src/App.js
@@ -0,0 +1,24 @@
+import react from "react";
+
+export default class App extends react.Component{
+  render (){
+    return (
+      <div>
+        <div>
+          
+        </div>
+
+        <div>
+
+        </div>
+  
+        <div>
+  
+        </div>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
