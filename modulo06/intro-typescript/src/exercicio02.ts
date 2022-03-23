6639a86fddebdbf4340f0658d5a813e94743d1f4
Matheus Grativol <matheus_grati@hotmail.com>
2022-03-22 21:54:09 -0300

    Exercicio Introdução a TS

---
+++ modulo06/intro-typescript/src/exercicio02.ts
@@ -0,0 +1,15 @@
+// const cor01: string = process.agv[2]
+// const cor02: string = process.agv[3]
+// const cor03: string = process.agv[4]
+
+function imprimeTresCoresFavoritas(): void {
+    const cor1: string = "Azul"
+    const cor2: string = "Verde"
+    const cor3: string = "Vermelho"
+    // const cor1 = prompt("Insira sua primeira cor favorita")
+    // const cor2 = prompt("Insira sua segunda cor favorita")
+    // const cor3 = prompt("Insira sua terceira cor favorita")
+    console.log([cor1, cor2, cor3])
+}
+
+imprimeTresCoresFavoritas()
