d21d3baca4b5f5dffd940f272af14e68a2cd08b6
Matheus Grativol <matheus_grati@hotmail.com>
2022-04-26 19:10:43 -0300

    algo q nao lembro

--- modulo06/projeto-ToDoList/Mysql-Projeto-ToDoList.sql
+++ modulo06/projeto-ToDoList/Mysql-Projeto-ToDoList.sql
@@ -1,8 +1,21 @@
-USE `419859-matheus-souza`;
+SELECT * FROM User;
 
-CREATE TABLE User(
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE User
+SET name = "Astro",
+nickName = "astro"
+WHERE id = "c46b3c90-89ae-468d-8f7b-9674469051d1";
+
+CREATE TABLE Task_user (
 	id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    nickName VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+	title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    limitDate DATE NOT NULL,
+    creatorUserId VARCHAR(255),
+    FOREIGN KEY (creatorUserId) REFERENCES User(id)
 );
+
+SELECT * FROM Task_user;
+
+DELETE FROM Tasks
