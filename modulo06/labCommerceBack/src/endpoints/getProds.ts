6781077ef5e3fd09eb24487751b5f8f6656668ab
Matheus Grativol <matheus_grati@hotmail.com>
2022-04-28 21:53:55 -0300

    configurando...

---
+++ modulo06/labCommerceBack/src/endpoints/getProds.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+export async function getProds (req: Request, res: Response): Promise<void> {
+    try{}catch(error : any){}
+}
---
+++ modulo06/labCommerceBack/src/endpoints/getPurchases.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+export async function getPurcheses (req: Request, res: Response): Promise<void> {
+    try{}catch(error : any){}
+}
